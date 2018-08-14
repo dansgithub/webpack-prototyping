42233dd041169c82cc70b4c83008b34acb83b339
Niemann <Daniel.Niemann@eventim.de>
2018-08-14 11:19:53 +0200

    added p5

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+import * as p5 from 'p5';
+
+let s = (sk) => {   
+    sk.setup = () => {
+        sk.createCanvas(window.innerWidth, window.innerHeight);
+        sk.background(40);
+        sk.stroke(200,100,0);
+        
+    }
+}
+
+const P5 = new p5(s);
+ 
